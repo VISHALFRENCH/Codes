5aaf0065f3e90e9a92aa52aa1a9f24f7287bf3fb
VISHALFRENCH <82657568+VISHALFRENCH@users.noreply.github.com>
2021-04-24 20:16:24 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,56 @@
+package week2.day3;
+
+public class Calculator {
+
+	public void add(int num1, int num2) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the number to add: " );
+		System.out.println(num1+num2);
+		
+	}
+
+	public void add1(int num3, int num4, int num5) {
+		System.out.println("enter the number to add:");
+		System.out.println(num3+num4+num5);
+	}
+
+	public void mul(int N1, int N2) {
+		// TODO Auto-generated method stub
+		System.out.println(" enter the numbers to multiply:");
+		System.out.println(N1*N2);
+
+	}
+
+	public void mul1(int a1, double f1) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the numbers to multiply: ");
+		System.out.println(a1*f1);
+
+	}
+
+	public void sub(int x1, int y1) {
+		// TODO Auto-generated method stub
+		System.out.println(" enter the numbers to sub:" );
+		System.out.println(x1 - y1);
+	}
+
+	public void sub1(double x1, double y1) {
+		// TODO Auto-generated method stub
+		System.out.println("enter the umbers to sub:");
+		System.out.println(x1 - y1);
+	}
+
+	public void divide(int b1, int b2) {
+		// TODO Auto-generated method stub
+		System.out.println(" enter the numbers to divide");
+		System.out.println(b1/b2);
+		
+	}
+	public void divide2(double x1, int e1) {
+		// TODO Auto-generated method stub
+		System.out.println(" enter the numbers to divide");
+		System.out.println(x1/e1);
+
+	}
+	
+}
---
+++ Calculatorchild.java
@@ -0,0 +1,23 @@
+package week2.day3;
+
+public class Calculatorchild extends Calculator {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		Calculatorchild sm = new Calculatorchild();
+		
+		
+		sm.add(6,7);
+		sm.add1(5, 4, 6);
+		sm.sub(2, 3);
+		sm.sub1(5.6, 5.3);
+		sm.divide(23, 35);
+		sm.divide2(5.6, 23);
+		sm.mul(12, 23);
+		sm.mul1(5, 5.6);
+		sm.divide(1, 2);
+		sm.divide2(6.5,  23);
+	}
+
+}
