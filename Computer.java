6575b5edc6c8163ac2d254a3b489bb896302fd2b
VISHALFRENCH <82657568+VISHALFRENCH@users.noreply.github.com>
2021-04-25 09:47:15 +0530

    Add files via upload

---
+++ Computer.java
@@ -0,0 +1,9 @@
+package org.system;
+
+public class Computer {
+
+	public void computermode() {
+		// TODO Auto-generated method stub
+		System.out.println(" computer mode is: ");
+	}
+}
---
+++ Desktop.java
@@ -0,0 +1,20 @@
+package org.system;
+
+public class  Desktop extends Computer {
+	public void desktopsize() {
+		// TODO Auto-generated method stub
+ System.out.println(" desktop size is :");
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+	  Desktop sm = new Desktop() ;
+	  
+	  sm.desktopsize();
+	  sm.computermode();
+	  
+
+	}
+
+}
